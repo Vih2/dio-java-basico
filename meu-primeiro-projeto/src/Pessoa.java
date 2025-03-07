ec6cf74e63c7e7bcd8c277648e3ae70abc2c60ac
Vih2 <vilmaomb@gmail.com>
2025-03-07 20:29:03 -0300

    criando classe pessoa

---
+++ meu-primeiro-projeto/src/Pessoa.java
@@ -0,0 +1,12 @@
+public class Pessoa {
+    String nome = "Maria";
+    int idade = 21;
+    String cor = "Branca";
+    public Pessoa(String nome, int idade, String cor) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cor = cor;
+    }
+    
+
+}
