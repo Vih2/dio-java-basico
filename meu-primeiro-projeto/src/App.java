e33a49fbc0886d4534dbc322249cd334779e01c0
Vih2 <vilmaomb@gmail.com>
2025-03-07 20:31:42 -0300

    printando classe pessoa

--- meu-primeiro-projeto/src/App.java
+++ meu-primeiro-projeto/src/App.java
@@ -1,5 +1,12 @@
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello filho!");
+       Pessoa bruna = new Pessoa("joana" , 21, "preta");
+       System.out.println(bruna.cor);
+       System.out.println(bruna.idade);
+       System.out.println(bruna.nome);
+
     }
 }
+
